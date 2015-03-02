be5320cd84a62aaca54847742f2f9fde4280571e
Pablo Ruiz <pjruiz@maquech.com.mx>
2015-03-01 20:29:17 -0600

    bug al requerir modulo certificación

--- lib/MX/SAT.rb
+++ lib/MX/SAT.rb
@@ -3,6 +3,6 @@ require 'simple_xlsx_reader'
 require 'nokogiri'
 
 require "MX/SAT/version"
-require "MX/SAT/Certificacion"
+require "MX/SAT/certificacion"
 require "MX/SAT/Contabilidad/balanza_comprobacion"
 require "MX/SAT/Contabilidad/catalogo_cuentas"
